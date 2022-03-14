e52aac23d1e79168f3be0b482f176c724f07da8c
Nelson Rosales <nelson.rosales83@gmail.com>
2022-03-14 21:59:21 +0100

    redux installed

---
+++ src/redux-og.ts
@@ -0,0 +1,8 @@
+// constants
+const CREATE_TODO = "CREATE_TODO";
+const EDIT_TODO = "EDIT_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const DELETE_TODO = "DELETE_TODO";
+const SELECT_TODO = "SELECT_TODO";
+
+// Actions & Action Type
